76d02c8f882fef92d28f169f9c60779482b57772
Danny Hayes <danielhayes5@gmail.com>
2023-11-08 18:08:26 -0500

    test commit

--- packages/document-parser/src/index.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import pdf from 'pdf-parse';
-
-export async function parsePdf(filePath: string) {
-  const file = await fs.promises.readFile(filePath);
-  const data = await pdf(file);
-  return data.text;
-}
